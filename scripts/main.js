9fc1ce8d05f85c35fd63ea8d4a5f7a6f1f070162
Bryant-Reynoso <52264330+Bryant-Reynoso@users.noreply.github.com>
2022-08-30 23:51:21 -0500

    added some basic redirect functionality and placeholders and stylings

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,17 @@
-console.log("test");
+
+function init(){
+	initEvents();
+}
+
+function initEvents(){
+	var redirectElementList = document.querySelectorAll("[data-redirect]");
+		
+	redirectElementList.forEach((element)=>{
+		element.addEventListener("click", (event)=>{
+			var redirectValue = event.target.getAttribute("data-redirect");
+			window.location.href = redirectValue;
+		});
+	});
+	
+}
+
